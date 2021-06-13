26b31eaadd2954b3ac59ffab4a0f83b1f2629c0f
Emerson Claudelino <48636325+ebilieri@users.noreply.github.com>
2021-06-13 18:32:30 -0300

    PasswordValidate sealed class

--- src/ValidaSenhaApi/Services/PasswordValidate.cs
+++ src/ValidaSenhaApi/Services/PasswordValidate.cs
@@ -7,16 +7,49 @@ namespace ValidaSenhaApi.Services
     /// <summary>
     /// PasswordValidate
     /// </summary>
-    public class PasswordValidate : Password
+    public sealed class PasswordValidate
     {
-        // Protected Methods
+
+        /// <summary>
+        /// Valitate Password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public bool Validate(string password)
+        {
+
+            //Nove ou mais caracteres
+            if (NimeOrMoreCharacters(password) == false) return false;
+
+            //Ao menos 1 dígito
+            if (OneOrMoreDigits(password) == false) return false;
+
+            //Ao menos 1 letra minúscula
+            if (OneOrMoreLowerCaseLetter(password) == false) return false;
+
+            //Ao menos 1 letra maiúscula
+            if (OneOrMoreUpperCaseLetter(password) == false) return false;
+
+            //Ao menos 1 caractere especial !@#$%^&*()-+
+            if (SpecialCharacter(password) == false) return false;
+
+            //Não possuir caracteres repetidos dentro do conjunto
+            if (RepeatedCharactersWithinTheSet(password) == false) return false;
+
+            //Espaços em branco não devem ser considerados como caracteres válidos.
+            return NotSpacesCharacters(password);
+        }
+
+
+
+        // Private Methods
 
         /// <summary>
         /// Validar quantidade de caracteres
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool NimeOrMoreCharacters(string password)
+        private bool NimeOrMoreCharacters(string password)
         {
             return RegexHelpers.nineOrMoreCharacters.IsMatch(password);
         }
@@ -26,7 +59,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool NotSpacesCharacters(string password)
+        private bool NotSpacesCharacters(string password)
         {
             //bool s  = RegexHelpers.notSpacesCharacters.IsMatch(password);
             // return false se conter caracater em branco (!) 
@@ -38,7 +71,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool OneOrMoreDigits(string password)
+        private bool OneOrMoreDigits(string password)
         {
             return RegexHelpers.oneOrMoreDigits.IsMatch(password);
         }
@@ -48,7 +81,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool OneOrMoreLowerCaseLetter(string password)
+        private bool OneOrMoreLowerCaseLetter(string password)
         {
             return RegexHelpers.oneOrMoreLowerCaseLetter.IsMatch(password);
         }
@@ -58,7 +91,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool OneOrMoreUpperCaseLetter(string password)
+        private bool OneOrMoreUpperCaseLetter(string password)
         {
             return RegexHelpers.oneOrMoreUpperCaseLetter.IsMatch(password);
         }
@@ -68,7 +101,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool RepeatedCharactersWithinTheSet(string password)
+        private bool RepeatedCharactersWithinTheSet(string password)
         {
             return password?.CountBy(c => c).Where(kvp => kvp.Value > 1).Count() == 0;
         }
@@ -78,7 +111,7 @@ namespace ValidaSenhaApi.Services
         /// </summary>
         /// <param name="password"></param>
         /// <returns></returns>
-        protected override bool SpecialCharacter(string password)
+        private bool SpecialCharacter(string password)
         {
             return RegexHelpers.specialCharactersRegex.IsMatch(password);
         }
--- src/ValidaSenhaApi/Services/PasswordValidateService.cs
+++ src/ValidaSenhaApi/Services/PasswordValidateService.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// 
     /// </summary>
-    public class PasswordValidateService : PasswordValidate, IPasswordValidateService
+    public class PasswordValidateService :  IPasswordValidateService
     {
         /// <summary>
         /// Valitate Password
@@ -12,41 +12,8 @@
         /// <returns></returns>
         public bool Validate(string password)
         {
-
-            //List<bool> validate = new List<bool>
-            //{
-            //    NimeOrMoreCharacters(password),
-            //    OneOrMoreDigits(password)
-            //};
-
-            //bool b = !validate.Contains(false);
-
-            //Nove ou mais caracteres
-            bool valid = NimeOrMoreCharacters(password);
-            if (valid == false) return false;
-
-            //Ao menos 1 dígito
-            valid = OneOrMoreDigits(password);
-            if (valid == false) return false;
-
-            //Ao menos 1 letra minúscula
-            valid = OneOrMoreLowerCaseLetter(password);
-            if (valid == false) return false;
-
-            //Ao menos 1 letra maiúscula
-            valid = OneOrMoreUpperCaseLetter(password);
-            if (valid == false) return false;
-
-            //Ao menos 1 caractere especial !@#$%^&*()-+
-            valid = SpecialCharacter(password);
-            if (valid == false) return false;
-
-            //Não possuir caracteres repetidos dentro do conjunto
-            valid = RepeatedCharactersWithinTheSet(password);
-            if (valid == false) return false;
-
-            //Espaços em branco não devem ser considerados como caracteres válidos.
-            return NotSpacesCharacters(password);
+            PasswordValidate passwordValidate = new PasswordValidate();
+            return passwordValidate.Validate(password);
         }
     }
 }
